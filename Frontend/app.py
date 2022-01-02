224c0ff22e7afb9ed6acf665ffc309e6207dec5c
ahmedsaleem123 <2020340@student.cct.ie>
2022-01-02 16:16:42 +0000

    Add HTML file and display the list of books

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, jsonify
+import json
+import requests
 
 app = Flask(__name__)
 
 
+# referred to the link - https://www.geeksforgeeks.org/flask-creating-first-simple-application/
+
 @app.route('/')
 def default():
-    return render_template('listOfBooks.html')
+    # https://stackoverflow.com/questions/48189684/how-to-parse-json-array-of-objects-in-python
+    response = json.loads(requests.get("http://localhost:3000/api/books").text)
+    return render_template('listOfBooks.html', output_data=response)
 
 
 if __name__ == '__main__':
